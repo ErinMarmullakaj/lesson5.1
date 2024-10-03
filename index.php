d918e58e159a19c4bb233b5c08c988db6634c246
ErinMarmullakaj <erin.marmullakaj2@gmail.com>
2024-10-03 17:48:04 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+// $x = 20;
+
+// function variablat(){
+//     $y=15;
+//     echo"$y";
+// }
+// variablat();
+
+// echo"$x"."\n";
+
+// $x =5;
+// $y =7;
+
+// function sum(){
+//     global $x , $y;
+//     $y = $x +  $y;
+// }
+
+// sum();
+// echo $y;
+
+// function test(){
+//     static $y=0;
+//     echo "$y";
+//     $y++;
+// }
+
+// test();
+
+
+$sport1 = "football" ;
+$sport2 = "basketball";
+$sport3 = "marbles";
+$sport4 = "meq";
+$sportys = array ('football','basketball','marbles','meq');
+
+echo end($sportys)."<br>";
+echo count($sportys)."<br>";
+
+$len = count($sportys);
+
+for($i; $i<=len; $i++ )
+
+?>
